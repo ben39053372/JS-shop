009b1be43a35a20e9f38dd90e5971dcb17a64ed0
ben39053372 <ben39053372@gmail.com>
2021-05-09 23:36:55 +0800

    lesson 1

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,44 @@
 import '../styles/globals.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Navbar, Container, Nav } from 'react-bootstrap'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar expand="lg" variant="light" bg="light">
+        <Container>
+          <Navbar.Brand href="#">Shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav"></Navbar.Toggle>
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/about">Abouts</Nav.Link>
+              <Nav.Link href="/products">Products</Nav.Link>
+              <Nav.Link href="/kits">Kits</Nav.Link>
+              <Nav.Link href="/news">News</Nav.Link>
+              <Nav.Link href="/contact">Contact</Nav.Link>
+            </Nav>
+            <Nav>
+              <Navbar.Text>
+                usd
+              </Navbar.Text>
+              <Navbar.Text>
+                icon 1
+              </Navbar.Text>
+              <Navbar.Text>
+                icon 2
+              </Navbar.Text>
+              <Navbar.Text>
+                icon 3
+              </Navbar.Text>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
