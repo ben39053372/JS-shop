39f4efd8e9ab4a6d3e88c0cf6737c718db118104
ben39053372 <ben39053372@gmail.com>
2021-06-13 19:59:13 +0800

    lesson 5

---
+++ components/LargeProductCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Image from "next/image";
+import Rate from "rc-rate";
+import "rc-rate/assets/index.css";
+
+export const LargeProductCard = (props) => {
+  return (
+    <div style={{ display: "flex" }}>
+      <Image src="/image/product1.png" width={100} height={100} />
+      <div>
+        <div>
+          <Rate allowHalf value={3.5} disabled />
+        </div>
+        <h5>{props.productName}</h5>
+        <span>${props.productPrice}</span>
+        <span>
+          <s>${props.productOriginPrice}</s>
+        </span>
+        <p>{props.desc}</p>
+      </div>
+    </div>
+  );
+};
